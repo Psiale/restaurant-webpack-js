ded5375785710e3a40f8f47ef20190521d552d5a
Alexis <psialedev@gmail.com>
2020-08-29 10:51:32 -0500

    Changed the font

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ module.exports = {
   mode: 'development',
   entry: [
     './src/index.js',
-    'font-awesome/scss/font-awesome.scss',
   ],
   devtool: 'inline-source-map',
   module: {
@@ -26,13 +25,6 @@ module.exports = {
         use: [
           'file-loader',
         ],
-      },
-      {
-        test: /font-awesome\.config\.js/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'font-awesome-loader' }
-        ]
       },
     ],
   },
